c96192d92265016147f27721eb6ea3c5d428cfd0
ad1mir2al <143758413+ad1mir2al@users.noreply.github.com>
2024-11-06 02:24:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+    <meta charset="utf-8"
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+</head>
+
+
+<body>
+
+<p> fdssdf</p>
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
+
+<?
+include('db/dbcon.php');
+
+
+?>
