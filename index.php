6504ba40005e36b9a6060e6f8e8d70eb77fa16c5
ad1mir2al <143758413+ad1mir2al@users.noreply.github.com>
2024-11-06 02:29:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>
+ 	</title>
+ </head>
+ <body>
+ <p>Привет! Это мой Сайт</p>
+ <p1>Вот мое расписание:</p1>
+ <span>1. Покушать</span>
+ <span>2. Поспать</span>
+ </body>
+ </html>
